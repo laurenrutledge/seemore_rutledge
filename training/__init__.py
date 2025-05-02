98d5a8d65687f5c842a3b8debd5690c98827d198
Lauren Rutledge <lhr13@georgetown.edu>
2025-05-01 19:26:11 -0700

    adding function header for __init__

--- training/__init__.py
+++ training/__init__.py
@@ -1,3 +1,13 @@
+"""
+__init__.py
+
+This package installer exposes the top-level training entry point for the Seemore Vision-Language Model
+that was expanded / optimized by Lauren Rutledge for the sake of the seemore interview assignment.
+
+Author: Lauren Rutledge
+Date: April 2025
+"""
+
 
 # Making the train_model function available at the package level: 
 from training.train_model import train_model
